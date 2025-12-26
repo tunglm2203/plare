a01589e466a3426085337aa6f1cf6748418b654a
Tung Luu <tungluu2203@kaist.ac.kr>
2025-12-26 19:22:42 +0900

    Update bash scripts to get VLM labels

--- bash_scripts/script_generate_vlm_preference.sh
+++ bash_scripts/script_generate_vlm_preference.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 python scripts/generate_vlm_preference.py --env_name "mw_drawer-open-v2"
+python scripts/generate_vlm_preference.py --env_name "mw_sweep-into-v2"
+python scripts/generate_vlm_preference.py --env_name "mw_plate-slide-v2"
+python scripts/generate_vlm_preference.py --env_name "mw_door-open-v2"
